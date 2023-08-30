04bfe613d3cffe1d746f913f61cef745d4cbc449
Zawar <zedd@Zawars-MacBook-Pro.local>
2023-08-30 16:42:47 -0400

    Code completion

--- lib/modern_alert_dialog.dart
+++ lib/modern_alert_dialog.dart
@@ -1,7 +1,133 @@
 library modern_alert_dialog;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class ModernAlertDialog {
+  alertDialogInfo(
+    BuildContext context, {
+    required String title,
+    required String message,
+    Color? color,
+  }) {
+    bool isDarkMode = MediaQuery.of(context).platformBrightness == Brightness.dark;
+
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        backgroundColor: isDarkMode
+            ? const Color.fromARGB(255, 30, 30, 30)
+            : const Color.fromARGB(255, 249, 249, 249),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        title: Text(title),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              message,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 40),
+            InkWell(
+              onTap: () => Navigator.pop(context),
+              child: Container(
+                height: 30,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: color ?? Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Text(
+                  "OK",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  alertDialogInfoButton(
+    BuildContext context, {
+    required String title,
+    required String message,
+    required Function() onPressed,
+    Color? color,
+  }) {
+    bool isDarkMode = MediaQuery.of(context).platformBrightness == Brightness.dark;
+
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        backgroundColor: isDarkMode
+            ? const Color.fromARGB(255, 30, 30, 30)
+            : const Color.fromARGB(255, 249, 249, 249),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        title: Text(title),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              message,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 40),
+            InkWell(
+              onTap: onPressed,
+              child: Container(
+                height: 30,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: color ?? Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Text(
+                  "OK",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 6),
+            InkWell(
+              child: Container(
+                height: 30,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color:
+                      color?.withOpacity(0.15) ?? Theme.of(context).primaryColor.withOpacity(0.15),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Text(
+                  "Cancel",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              onTap: () => Navigator.pop(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- test/modern_alert_dialog_test.dart
+++ test/modern_alert_dialog_test.dart
@@ -1,12 +1,20 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:modern_alert_dialog/modern_alert_dialog.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('Creates alert dialog', () {
+    // Getting context does not work
+    final context = WidgetsBinding.instance.buildOwner!.focusManager.rootScope.context;
+    final modernAlertDialog = ModernAlertDialog();
+    expect(
+      modernAlertDialog.alertDialogInfo(
+        context!,
+        title: "Alert",
+        message: "Modern alert dialog",
+      ),
+      ModernAlertDialog(),
+    );
   });
 }
